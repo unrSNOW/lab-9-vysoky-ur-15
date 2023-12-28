b81fff566f412f64ea0cb0af995669622ee1c6bb
unrSNOW <Yaroslav.sapon@bk.ru>
2023-12-28 15:44:25 +0300

    Добавьте файлы проекта.

---
+++ lab 9 vysoky ur 15/Program.cs
@@ -0,0 +1,40 @@
+﻿class Employee
+{
+    public string LastName { get; set; }
+    public double Salary { get; set; }
+    public int StartYear { get; set; }
+
+    public Employee(string lastName, double salary, int startYear)
+    {
+        LastName = lastName;
+        Salary = salary;
+        StartYear = startYear;
+    }
+
+    public int CalculateExperience()
+    {
+        int currentYear = DateTime.Now.Year;
+        return currentYear - StartYear;
+    }
+
+    public int CalculateDaysSinceStartYear()
+    {
+        DateTime startDate = new DateTime(StartYear, 1, 1);
+        TimeSpan timeSpan = DateTime.Now - startDate;
+        return timeSpan.Days;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Employee employee = new Employee("Иванов", 50000, 2015);
+
+        int experience = employee.CalculateExperience();
+        Console.WriteLine("Стаж работы: " + experience + " лет");
+
+        int daysSinceStartYear = employee.CalculateDaysSinceStartYear();
+        Console.WriteLine("Прошло дней с года поступления на работу: " + daysSinceStartYear + " дней");
+    }
+}
